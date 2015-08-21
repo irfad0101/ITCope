d4e8ca292962c37d1da9b9517b4a4c3dfc15a7fa
HareenUdayanath <hareenudayanath.13@cse.mrt.ac.lk>
2015-08-21 15:01:54 +0530

    First4 8/21/2015

--- SemesterProject/src/DataBase/DBOperations.java
+++ SemesterProject/src/DataBase/DBOperations.java
@@ -19,7 +19,7 @@ public class DBOperations {
     private static DBOperations instance = null;
    
     //private String url = "jdbc:mysql://192.168.173.1:3306/SemesterProject";
-    private String ip = "192.168.173.1";
+    private String ip = "192.168.173.2";
     private String port = "3306";
     private String url = "jdbc:mysql://"+ip+":"+port+"/SemesterProject";
     //private String url = "jdbc:mysql://localhost:3306/SemesterProject";

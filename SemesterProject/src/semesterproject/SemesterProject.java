0f3072c34c3dece5177406d433aeffdc4b21ba1b
HareenUdayanath <hareenudayanath.13@cse.mrt.ac.lk>
2015-08-21 15:15:37 +0530

    First5 8/21/2015

--- SemesterProject/src/semesterproject/SemesterProject.java
+++ SemesterProject/src/semesterproject/SemesterProject.java
@@ -3,19 +3,13 @@ package semesterproject;
 
 import DataBase.*;
 import gui.login.LoginFace;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 
 public class SemesterProject {
 
    
     public static void main(String[] args) {
-       
-            DBOperations ad = DBOperations.getInstace();
-            
-
-           
+                 
             try {
                  for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                      if ("Nimbus".equals(info.getName())) {
@@ -27,18 +21,7 @@ public class SemesterProject {
 
              }
              LoginFace login = new LoginFace();
-             login.setVisible(true);
-
+             login.setVisible(true);            
             
-             //Help.writeIPandPort("123","123");
-             //String[] i = Help.readIPandPort();
-             //if(i[0]!=null)
-                 //System.out.println(i[0]+" "+i[1]);
-             //System.out.println("sdasdasd  "+Help.getDate(2010,05,06));*/
-        /*try {
-            System.out.println(ad.checkLabTecID("4"));
-        } catch (ConnectionTimeOutException ex) {
-            Logger.getLogger(SemesterProject.class.getName()).log(Level.SEVERE, null, ex);
-        }*/
     }
 }
